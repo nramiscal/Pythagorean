27a5ae3c45670f32e3598787947c3d04ce277b63
nramiscal <nramiscal@gmail.com>
2017-08-29 13:29:52 -0400

    Initial commit

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+  public static void main(String[] args) {
+    Pythagorean pyth = new Pythagorean();
+    double hypotenuse = pyth.calculateHypotenuse(2,3);
+    System.out.println(hypotenuse);
+  }
+}
